3f76431a0b7fbc0fc0c90ebcfd46fda6adc56a92
Alex Drysdale <alexander.drysdale@wales.nhs.uk>
2024-11-15 17:03:51 +0000

    Lint with ruff

--- popleave/popleave.py
+++ popleave/popleave.py
@@ -1,37 +1,37 @@
 #! python
 
-"""Populates a leave form from the command line"""
+"""Populates a leave form from the command line."""
+
+from __future__ import annotations
 
 # Python imports
 import argparse
 import datetime
-from importlib import resources
 import json
-import os
-import platform
 import sys
-from typing import Any, Optional
+from importlib import resources
+from pathlib import Path
 
 # Module imports
 import appdirs
 from docx import Document
 
 
-def _get_response(k: str, v: Any) -> Any:
+def _get_response(k: str, v: str | float) -> str | float:
     happy_with_response = False
     while not happy_with_response:
         user_input = input(f"Enter {k} (press Enter to use {v}): ")
         user_input = user_input if user_input else v
         response = input(
             f"Is {user_input} correct? "
-            "y to continue, n to re-enter or q to quit: "
+            "y to continue, n to re-enter or q to quit: ",
         )
         happy_with_response = (
-            response.lower() == 'y'
-            or response.lower() == 'yes'
-            or response.lower() == 'ydw'
+            response.lower() == "y"
+            or response.lower() == "yes"
+            or response.lower() == "ydw"
         )
-        if response.lower() == 'q' or response.lower() == 'quit':
+        if response.lower() == "q" or response.lower() == "quit":
             sys.exit()
     return user_input
 
@@ -40,11 +40,12 @@ def _format_date(iso_date: str) -> str:
 
 
 def init(
-        conf_path: Optional[str] = None,
+        conf_path: str | None  = None,
+        update_balance: int | None = None,
+        *,
         force_init: bool = False,
-        update_balance: Optional[int] = None,
 ) -> dict:
-    """Initilise config file
+    """Initilise config file.
 
     If the file already exists, simply returns the config
     file as a dictionary.
@@ -61,16 +62,16 @@ def init(
 
     Returns:
         config (dict) : Configuration dictionary.
+
     """
-
     if conf_path is None:
-        conf_dir = appdirs.user_config_dir('popleave')
-        conf_path = os.path.join(conf_dir, "config.json")
+        conf_dir = Path(appdirs.user_config_dir("popleave"))
+        conf_path = conf_dir / "config.json"
 
-        if not os.path.exists(conf_dir):
-            os.makedirs(conf_dir)
+        if not conf_dir.exists():
+            conf_dir.make_dirs()
 
-    if not os.path.exists(conf_path) or force_init:
+    if not conf_path.exists() or force_init:
 
         config = {
             "name": "Samwise Gamgee",
@@ -81,15 +82,15 @@ def init(
         for k, v in config.items():
             config[k] = _get_response(k, v)
 
-        with open(conf_path, "w+", encoding = "utf-8") as fp:
+        with conf_path.open("w+", encoding = "utf-8") as fp:
             json.dump(config, fp)
-            
+
     else:
-        with open(conf_path, "r", encoding = "utf-8") as fp:
+        with conf_path.open("r", encoding = "utf-8") as fp:
             config = json.load(fp)
 
         if update_balance is not None:
-            with open(conf_path, "w", encoding = "utf-8") as fp:
+            with conf_path.open("w", encoding = "utf-8") as fp:
                 config["remaing_days_leave"] = update_balance
                 json.dump(config, fp)
 
@@ -98,13 +99,14 @@ def init(
 
 def get_doc_dict(
         config: dict,
-        start_date: Optional[str | datetime.date] = None,
-        duration: Optional[int | float | datetime.timedelta] = None,
-        end_date: Optional[str | datetime.date] = None,
-        reason: Optional[str] = None,
+        start_date: str | datetime.date | None = None,
+        duration: float | datetime.timedelta | None = None,
+        end_date: str | datetime.date | None = None,
+        reason: str | None = None,
+        *,
         is_toil: bool = False,
 ) -> dict:
-    """Return populated document dictionary
+    """Return populated document dictionary.
 
     Args:
         config (dict): Config dictionary.
@@ -124,19 +126,21 @@ def get_doc_dict(
 
     Returns:
         doc_dict (dict) : Dictionary of documentation fields.
-    """
 
-    doc_dict = dict()
+    """
+    doc_dict = {}
+
+    date_today = str(datetime.datetime.now(tz=datetime.timezone.utc).date())
 
     if start_date is None:
         start_date = _get_response(
-            'Start date (YYYY-MM-DD)', str(datetime.date.today()),
+            "Start date (YYYY-MM-DD)", date_today,
         )
     start_date = datetime.date.fromisoformat(start_date)
-    
+
     if duration is None and end_date is None:
-        duration = int(_get_response('number of days', 1))
-    
+        duration = int(_get_response("number of days", 1))
+
     if duration is None:
         duration = datetime.data.fromisoformat(end_date) - start_date
     else:
@@ -146,91 +150,86 @@ def get_doc_dict(
         end_date = start_date + duration
 
     if reason is None:
-        reason = _get_response('reason for leave', 'Holiday')
+        reason = _get_response("reason for leave", "Holiday")
 
     # Of the format (value, (table, row, col)))
-    doc_dict['name'] = (config['name'], (0, 0, 0))
-    doc_dict['department'] = (config['department'], (0, 0, 1))
-    doc_dict['today'] = (
-        _format_date(str(datetime.date.today())),
+    doc_dict["name"] = (config["name"], (0, 0, 0))
+    doc_dict["department"] = (config["department"], (0, 0, 1))
+    doc_dict["today"] = (
+        _format_date(date_today),
         (0, 0, 2),
     )
-    
+
     type_row = 3 if is_toil else 1
-    doc_dict['type'] = ('x', (1, type_row, 1))
-    
-    doc_dict['start_date'] = (
+    doc_dict["type"] = ("x", (1, type_row, 1))
+
+    doc_dict["start_date"] = (
         _format_date(str(start_date)),
         (2, 3, 0),
     )
-    doc_dict['end_date'] = (
+    doc_dict["end_date"] = (
         _format_date(str(end_date)),
         (2, 3, 1),
     )
-    doc_dict['duration'] = (duration.days, (2, 3, 2))
+    doc_dict["duration"] = (duration.days, (2, 3, 2))
     if is_toil:
-        balance = config['remaing_days_leave']
+        balance = config["remaing_days_leave"]
     else:
-        balance = int(config['remaing_days_leave']) - duration.days
-    doc_dict['balance'] = (balance, (2, 3, 3))
-        
-    doc_dict['reason'] = (str(reason), (3, 0, 0))
+        balance = int(config["remaing_days_leave"]) - duration.days
+    doc_dict["balance"] = (balance, (2, 3, 3))
+
+    doc_dict["reason"] = (str(reason), (3, 0, 0))
 
     return doc_dict
 
 
 def populate_file(
         doc_dict: dict,
-        template_file: Optional[str] = None,
+        template_file: str | None = None,
 ) -> bool:
-    """Populate a word template file with annual leave info
+    """Populate a word template file with annual leave info.
 
     Args:
         doc_dict (dict) : Dictionary of leave info.
         template_file (str, optional) : Template file to populate.
                 If None, will load the template.docx file associated with
                 the package. Defaults to None.
-    
+
     Returns:
         return_val (bool) : True if successful, False otherwise.
-    """
 
-    my_package = (
-        os.path.splitext(os.path.basename(__file__))[0]
-        if not __package__
-        else __package__
-    )
+    """
+    my_package = __package__ if __package__ else Path(__file__).stem
     template_file = (
-        resources.files(my_package).joinpath('template.docx')
+        resources.files(my_package).joinpath("template.docx")
         if template_file is None
         else template_file
     )
-    
+
     doc = Document(template_file)
     for value, (t, r, c) in doc_dict.values():
         doc.tables[t].cell(r, c).text = (
             f"{doc.tables[t].cell(r, c).text}\n{value}"
         )
 
-    initials = "".join([n[0].upper() for n in doc_dict['name'][0].split()])
-    startday = "".join(doc_dict['start_date'][0].split("/"))
+    initials = "".join([n[0].upper() for n in doc_dict["name"][0].split()])
+    startday = "".join(doc_dict["start_date"][0].split("/"))
 
-    
+
     new_file_name = f"{initials}_ANNUAL_{startday}.docx"
     doc.save(new_file_name)
 
     new_config = init(update_balance=int(doc_dict["balance"][0]))
 
-    print(f"You have {new_config['remaing_days_leave']} days holiday remaining.")
-    
+    print(f"You have {new_config["remaing_days_leave"]} days holiday remaining.")
+
     return True
 
 
 def main(
-        force_init: bool = False, is_toil: bool = False, **kwargs: Any,
+        *, force_init: bool = False, is_toil: bool = False, **kwargs: object,
 ) -> bool:
-    """Main script"""
-    
+    """Script for populating the leave request form."""
     config = init(force_init=force_init)
     doc_dict = get_doc_dict(config, is_toil=is_toil, **kwargs)
     return populate_file(doc_dict)
@@ -241,61 +240,61 @@ if __name__ == "__main__":
     #####################
     # Formats Arguments #
     #####################
-    
+
     parser = argparse.ArgumentParser(
-        description='Populates the leave of absence form with a CLI',
+        description="Populates the leave of absence form with a CLI",
         allow_abbrev=True,
     )
 
     parser.add_argument(
-        '-i',
-        '--init',
-        action='store_true',
-        help='Forces reinitialisation of config file.',
+        "-i",
+        "--init",
+        action="store_true",
+        help="Forces reinitialisation of config file.",
     )
 
     parser.add_argument(
-        '-s',
-        '--start_date',
+        "-s",
+        "--start_date",
         type=str,
         default=None,
-        help='Start date in YYYY-MM-DD format.'
+        help="Start date in YYYY-MM-DD format.",
     )
 
     parser.add_argument(
-        '-e',
-        '--end_date',
+        "-e",
+        "--end_date",
         type=str,
         default=None,
-        help='End date in YYYY-MM-DD format.'
+        help="End date in YYYY-MM-DD format.",
     )
 
     parser.add_argument(
-        '-d',
-        '--duration',
+        "-d",
+        "--duration",
         type=int,
         default=None,
-        help='Duration in days.',
+        help="Duration in days.",
     )
 
     parser.add_argument(
-        '-r',
-        '--reason',
+        "-r",
+        "--reason",
         type=str,
         default=None,
-        help='Reason for leave.',
+        help="Reason for leave.",
     )
 
     parser.add_argument(
-        '-t',
-        '--toil',
-        action='store_true',
-        help='Fills out Time Off In Leu instead of annual leave.',
+        "-t",
+        "--toil",
+        action="store_true",
+        help="Fills out Time Off In Leu instead of annual leave.",
     )
 
     args = vars(parser.parse_args())
-    force_init = args.pop('init')
-    is_toil = args.pop('toil')
+    force_init = args.pop("init")
+    is_toil = args.pop("toil")
 
     #####################
     # Runs main program #
